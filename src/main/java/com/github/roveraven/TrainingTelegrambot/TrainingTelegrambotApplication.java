19289f8bc0eb4992e34a95705f657c722a14470c
RoveRaven <kraven88@yandex.ru>
2023-01-14 22:44:20 +0300

    RRTB-0 initial commit

---
+++ src/main/java/com/github/roveraven/TrainingTelegrambot/TrainingTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.roveraven.TrainingTelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingTelegrambotApplication.class, args);
+	}
+
+}
